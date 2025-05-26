d4e33796f78c4ea92662e8d9f684e691521a7bae
Luc <lisson.luc@gmail.com>
2025-05-26 22:16:17 +0200

    added regression and r-squared of albon lap times in monaco

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import fastf1 as fastf1
-
-
+from sklearn.metrics import r2_score
 
 session = fastf1.get_session(2025, 'Monaco', 'R')
 session.load()
@@ -22,12 +21,37 @@ alb_time = alb_car_data['Time']
 alb_df2 = alb_df[['LapTime', 'Sector1Time','Sector2Time', 'Sector3Time', 'TyreLife', 'IsAccurate']]
 alb_df2 = alb_df2.query("IsAccurate == True")
 
+plt.ylabel('seconds')
+plt.xlabel('laps')
 
 x_values = np.arange(1, alb_df2.get('IsAccurate').count() + 1 ,1)
 
+for col in ['LapTime', 'Sector1Time', 'Sector2Time', 'Sector3Time']:
+    alb_df2[col] = alb_df2[col].dt.total_seconds()
 
+'''
 plt.plot(x_values, alb_df2['LapTime'], label = 'LapTime')
 plt.plot(x_values, alb_df2['Sector1Time'], label = 'Sector1')
 plt.plot(x_values, alb_df2['Sector2Time'], label = 'Sector2')
 plt.plot(x_values, alb_df2['Sector3Time'], label = 'Sector3')
+'''
+plt.scatter(x_values,alb_df2['LapTime'])
+func = np.poly1d(np.polyfit(x_values,alb_df2['LapTime'],6))
+
+rSquared = r2_score(alb_df2['LapTime'], func(x_values))
+print(rSquared)
+plt.plot(x_values, func(x_values), c='r')
+plt.legend()
 plt.show()
+
+
+'''
+idee für morgen:
+- get_car data anschauen für ein fahrer
+- welche column haben am meisten einfluss auf den speed
+- unnötige data entfernen
+- model bauen die speed predicted
+
+multiple regression
+'''
+# %%
